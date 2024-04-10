245f787a774b8a30547811cb9a6c5acbcf04cd25
Samuel Huff <samuel.huff@ayahealthcare.com>
2024-04-10 14:08:51 -0700

    docs: document array quirks

--- src/app/facility/components/template/facility-phone-numbers-form/facility-phone-numbers-form.component.ts
+++ src/app/facility/components/template/facility-phone-numbers-form/facility-phone-numbers-form.component.ts
@@ -1,6 +1,13 @@
 import { Component, Input } from "@angular/core";
 import { FORMS, formContainerViewProvider } from "@app/shared/core";
 
+// Arrays can have a few "gotcha" moments with template driven. This is a straightforward demonstration using just an array of strings.
+// In this case, we lose the ability to do validation because the string object doesn't have any properties our ValidationSuite can map to.
+//
+// When handling an array of complex objects, things work just as expected with other objects. Use the
+// index to access the element in the array, and the modelType to map a validation suite.
+//
+// This blog post covers some other gotchas: https://blog.simplified.courses/template-driven-forms-with-form-arrays/
 @Component({
     selector: 'app-facility-phone-numbers-form',
     standalone: true,
