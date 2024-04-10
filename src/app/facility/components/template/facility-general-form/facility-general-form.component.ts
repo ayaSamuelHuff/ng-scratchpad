661e9444d8741e2208b42ececac9f92d0fe9f228
Samuel Huff <samuel.huff@ayahealthcare.com>
2024-04-10 13:35:38 -0700

    chore: separate phone form

--- src/app/facility/components/template/facility-general-form/facility-general-form.component.ts
+++ src/app/facility/components/template/facility-general-form/facility-general-form.component.ts
@@ -1,25 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Facility } from '@app/facility/models/facility.model';
 import { FORMS, formContainerViewProvider } from '@app/shared/core';
-
-@Component({
-  selector: 'app-facility-phone-numbers-form',
-  standalone: true,
-  imports: [FORMS],
-  template: `<div class="col" [ngModelGroup]="ngModelGroupName" [model]="vm" modelType="">
-@for (number of vm; track i; let i = $index) {
-  <div class="row">
-  <input-text  name="{{i}}" placeholder="Phone Number"></input-text>
-</div>
-}
-</div>
-`,
-  viewProviders: [formContainerViewProvider]
-})
-export class FacilityPhoneNumbersFormComponent {
-  @Input() vm?: Partial<string[]>;
-  @Input('name') ngModelGroupName = "phoneNumbers";
-}
+import { FacilityPhoneNumbersFormComponent } from '../facility-phone-numbers-form/facility-phone-numbers-form.component';
 
 @Component({
   selector: 'app-facility-general-form',
